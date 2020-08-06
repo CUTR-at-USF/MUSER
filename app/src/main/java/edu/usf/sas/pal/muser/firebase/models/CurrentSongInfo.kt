4c7927ea002110578d9200c127f07ed9cc899847
BumbleFlash <sudarshan97@gmail.com>
2020-08-06 15:28:37 -0400

    chore: added a kotlin class that stores information about the song that is currently being played.

---
+++ app/src/main/java/edu/usf/sas/pal/muser/firebase/models/CurrentSongInfo.kt
@@ -0,0 +1,32 @@
+package edu.usf.sas.pal.muser.firebase.models
+
+
+/**
+ * A model class that holds the information about the song that is currently being played by the
+ * user.
+ */
+data class CurrentSongInfo
+
+/**
+ * [id] - The id of the track.
+ * [name] - Name of the track.
+ * [artistName] - Name of the Artist who composed the track.
+ * [albumName] - Name of the album that the current track belongs to.
+ * [duration] - The length of the track.
+ * [dateAdded] - The date on which the track was added.
+ * [year] - The year during which the track was released.
+ * [playCount] - The number of times the user played this track.
+ * [sampleRateLabel] - The sample rate of the song.
+ * [formatLabel] - The format of the song. (mp3 or Flac etc.)
+ */
+(val id: Long,
+ val name: String,
+ val artistName: String,
+ val albumName: String,
+ val duration: Long,
+ val dateAdded: Int,
+ val year: Int,
+ val playCount: Int,
+ val sampleRateLabel: String,
+ val formatLabel: String
+)
