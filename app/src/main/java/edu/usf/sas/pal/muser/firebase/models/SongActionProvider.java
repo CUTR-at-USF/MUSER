ab0272f5246ccf37193fc30dad339b9d27039d1f
BumbleFlash <sudarshan97@gmail.com>
2020-08-07 18:48:58 -0400

    chore: added comments to SongActionProvider.java

--- app/src/main/java/edu/usf/sas/pal/muser/firebase/models/SongActionProvider.java
+++ app/src/main/java/edu/usf/sas/pal/muser/firebase/models/SongActionProvider.java
@@ -1,12 +1,16 @@
 package edu.usf.sas.pal.muser.firebase.models;
 
+/**
+ *  The provider class that stores different types of action that can be performed by the user
+ *  on a sound track.
+ */
 public interface SongActionProvider {
     @interface ActionType{
         String ACTION_PLAY = "Play";
         String ACTION_PAUSE = "Pause";
         String ACTION_SKIP = "Skip";
         String ACTION_REPEAT = "Repeat";
-        String ACTION_SEEK_TO = "SeekTo";
+        String ACTION_SEEK= "Seek";
     }
 
 }
