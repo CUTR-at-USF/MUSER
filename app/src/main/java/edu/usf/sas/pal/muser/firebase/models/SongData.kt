c064332475cb06703b13bdc7ef532549a7b6d27f
BumbleFlash <sudarshan97@gmail.com>
2020-08-10 13:51:46 -0400

    chore: added more properties to SongData.kt class.

--- app/src/main/java/edu/usf/sas/pal/muser/firebase/models/SongData.kt
+++ app/src/main/java/edu/usf/sas/pal/muser/firebase/models/SongData.kt
@@ -10,8 +10,16 @@ data class SongData
 /**
  * [id] - The id of the track.
  * [name] - Name of the track.
+ * [artistID] - The id of the artist.
  * [artistName] - Name of the Artist who composed the track.
+ * [albumID] - The id of the album.
  * [albumName] - Name of the album that the current track belongs to.
+ * [playlistID] - The id of the playlist that track belongs to.
+ * [playlistPlayOrder] - The queue number of the song in a playlist.
+ * [discNumber] - The disc number that the track belongs to.
+ * [bitrateLabel] - The bitrate of the song.
+ * [fileSizeLabel] - The file size of the song.
+ * [isPodCast] - Boolean value to determine if the current audio file is a pod cast or not.
  * [duration] - The length of the track.
  * [dateAdded] - The date on which the track was added.
  * [year] - The year during which the track was released.
@@ -21,12 +29,22 @@ data class SongData
  */
 (val id: Long,
  val name: String,
+ val artistID: Long,
  val artistName: String,
+ val albumID: Long,
  val albumName: String,
- val duration: Long,
- val dateAdded: Int,
- val year: Int,
- val playCount: Int,
- val sampleRateLabel: String,
- val formatLabel: String
+ val playlistID: Long,
+ val playlistPlayOrder: Long?,
+ val lastPlayed: Long?,
+ val track: Int?,
+ val discNumber: Int?,
+ val bitrateLabel: String?,
+ val fileSizeLabel: String?,
+ val isPodCast: Boolean?,
+ val duration: Long?,
+ val dateAdded: Int?,
+ val year: Int?,
+ val playCount: Int?,
+ val sampleRateLabel: String?,
+ val formatLabel: String?
 )
