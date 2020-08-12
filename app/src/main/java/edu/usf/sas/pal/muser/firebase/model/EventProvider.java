8331173ac7c2f616e255048dabb85765c33cecd7
BumbleFlash <sudarshan97@gmail.com>
2020-08-11 21:02:24 -0400

    chore: modified EventProvider.java class to be an enum class

--- app/src/main/java/edu/usf/sas/pal/muser/firebase/model/EventProvider.java
+++ app/src/main/java/edu/usf/sas/pal/muser/firebase/model/EventProvider.java
@@ -1,16 +1,24 @@
 package edu.usf.sas.pal.muser.firebase.model;
 
 /**
- *  The provider class that stores different types of action that can be performed by the user
+ *  The provider enumerator that stores different types of action that can be performed by the user
  *  on a sound track.
  */
-public interface EventProvider {
-    @interface EventType{
-        String EVENT_PLAY = "Play";
-        String EVENT_PAUSE = "Pause";
-        String EVENT_SKIP = "Skip";
-        String EVENT_REPEAT = "Repeat";
-        String EVENT_SEEK= "Seek";
-    }
+public enum EventProvider {
 
+        EVENT_PLAY("Play"),
+        EVENT_PAUSE("Pause"),
+        EVENT_SKIP("Skip"),
+        EVENT_REPEAT("Repeat"),
+        EVENT_SEEK("Seek");
+
+        private String event;
+
+        EventProvider(String event) {
+            this.event = event;
+        }
+
+        public String getEvent(){
+            return event;
+        }
 }
