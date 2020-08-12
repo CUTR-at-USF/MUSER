9c3b5a76259226c609f23f3dfd98e215f05ed78f
BumbleFlash <sudarshan97@gmail.com>
2020-08-12 13:03:20 -0400

    chore: Added comments to EventCaptureUtils.java

--- app/src/main/java/edu/usf/sas/pal/muser/util/EventCaptureUtils.java
+++ app/src/main/java/edu/usf/sas/pal/muser/util/EventCaptureUtils.java
@@ -8,14 +8,31 @@ import com.simplecity.amp_library.model.Song;
 import edu.usf.sas.pal.muser.firebase.model.Event;
 import edu.usf.sas.pal.muser.firebase.model.SongData;
 
+
+
 public class EventCaptureUtils {
+
+    /**
+     * Function to populate the Event data class.
+     * @param song - The song on which the event happened.
+     * @param capturedEvent - The event that was captured.
+     * @param context - The context of the Fragment.
+     * @return - Event object
+     */
     public static Event captureEvent(Song song, String capturedEvent, Context context){
         long a = 0;
         long b = 0;
         SongData songData = populateSongData(song, context);
+        // TODO populate currentTimeMs, NanoTime, startTime and elaspedTime.
         return new Event(capturedEvent, 0, 0, a,b, songData);
     }
 
+    /**
+     * Function to populate SongData class. Called by captureEvent().
+     * @param song - Details of the song.
+     * @param context - Context of the fragment.
+     * @return - SongData object
+     */
     public static SongData populateSongData(Song song, Context context){
         return new SongData(song.id, song.name, song.artistId, song.albumName, song.albumId
                 , song.albumName, song.playlistSongId, song.playlistSongPlayOrder, song.lastPlayed
