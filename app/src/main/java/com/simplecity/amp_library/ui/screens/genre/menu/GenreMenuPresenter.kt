9ad8216eb89a7b07ba4c87d4b8d68383a15dde79
BumbleFlash <sudarshan97@gmail.com>
2020-10-22 13:37:55 -0400

    chore: added UI_EVENT from GenreListFragment.

--- app/src/main/java/com/simplecity/amp_library/ui/screens/genre/menu/GenreMenuPresenter.kt
+++ app/src/main/java/com/simplecity/amp_library/ui/screens/genre/menu/GenreMenuPresenter.kt
@@ -1,6 +1,7 @@
 package com.simplecity.amp_library.ui.screens.genre.menu
 
 import android.content.Context
+import com.simplecity.amp_library.ShuttleApplication
 import com.simplecity.amp_library.model.Genre
 import com.simplecity.amp_library.model.Playlist
 import com.simplecity.amp_library.model.Song
@@ -9,10 +10,17 @@ import com.simplecity.amp_library.ui.common.Presenter
 import com.simplecity.amp_library.ui.screens.album.menu.AlbumMenuPresenter
 import com.simplecity.amp_library.utils.LogUtils
 import com.simplecity.amp_library.utils.extensions.getSongs
+import com.simplecity.amp_library.utils.extensions.getSongsSingle
 import com.simplecity.amp_library.utils.playlists.PlaylistManager
+import edu.usf.sas.pal.muser.model.UiEventType
+import edu.usf.sas.pal.muser.util.EventUtils
+import edu.usf.sas.pal.muser.util.FirebaseIOUtils
+import edu.usf.sas.pal.muser.util.SongUtils
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import java.util.*
 import javax.inject.Inject
+import kotlin.concurrent.schedule
 
 class GenreMenuPresenter @Inject constructor(
     private val context: Context,
@@ -45,6 +53,9 @@ class GenreMenuPresenter @Inject constructor(
         mediaManager.playAll(genre.getSongs(context)) {
             view?.onPlaybackFailed()
         }
+        Timer().schedule(200) {
+            newUiEvent(UiEventType.PLAY)
+        }
     }
 
     override fun playNext(genre: Genre) {
@@ -66,5 +77,11 @@ class GenreMenuPresenter @Inject constructor(
                 )
         )
     }
-
+    fun newUiEvent(uiEventType: UiEventType){
+        val song = SongUtils.getSong()
+        if (song != null) {
+            val uiEvent = EventUtils.newUiEvent(song, uiEventType, ShuttleApplication.get())
+            FirebaseIOUtils.saveUiEvent(uiEvent)
+        }
+    }
 }
